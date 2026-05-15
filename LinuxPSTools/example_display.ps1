5a8e66e6bc1dfd7908287466e76565b25389365d
petrj <petrjanousek.net@gmail.com>
2026-05-15 18:34:13 +0200

    RadI0 Display

---
+++ LinuxPSTools/example_display.ps1
@@ -0,0 +1,113 @@
+Set-Location $PSScriptRoot
+
+if (Get-Module -Name LinuxPSTools)
+{
+    Write-Host "Reloading LinuxPSTools module..."
+    Remove-Module LinuxPSTools
+}
+
+Import-Module .\LinuxPSTools.psd1
+
+Clear-TTYDisplay -DeviceName "/dev/ttyUSB0"
+Write-TTYDisplayText -DeviceName "/dev/ttyUSB0" -Text "Hello, World!" 
+
+$port = 5000
+
+$udpClient = New-Object System.Net.Sockets.UdpClient($port)
+$udpClient.Client.ReceiveTimeout = 1000
+
+Write-Host "Listening on UDP port $port ..."
+
+$radioName = "";
+$radioText = "";
+$radioFreq = "";
+$radioStatus = "";
+
+$LASTROW1 = "";
+$LASTROW2 = "";
+$LASTRADIOText = "";
+
+$row2Offset = 0;
+
+while ($true)
+{
+    $remoteEndPoint = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Any, 0)
+    $gotPacket = $false
+
+    try
+    {
+        $bytes = $udpClient.Receive([ref]$remoteEndPoint)
+        $gotPacket = $true
+    }
+    catch [System.Net.Sockets.SocketException]
+    {
+        if ($_.Exception.SocketErrorCode -ne [System.Net.Sockets.SocketError]::TimedOut)
+        {
+            throw
+        }
+    }
+
+    if ($gotPacket)
+    {
+        $json = [System.Text.Encoding]::UTF8.GetString($bytes)
+
+        # Parse JSON
+        $data = $json | ConvertFrom-Json
+
+        $radioStatus = $data.status
+        $radioFreq = $data.freq
+        $radioName = $data.name
+        $radioText = [string]$data.dynamicLabel
+
+        if ($radioText -ne $LASTRADIOText)
+        {
+            $row2Offset = 0
+            $LASTRADIOText = $radioText
+        }
+    }
+
+    if ($radioText.Length -gt 20)
+    {
+        $row2Offset = ($row2Offset + 1) % ($radioText.Length + 20)
+    }
+    else
+    {
+        $row2Offset = 0
+    }
+
+    $row1 = $radioName.PadRight(20)
+    if ($row1.length -gt 20)
+    {
+        $row1 = $row1.Substring(0, 20)
+    }
+
+    if ($radioText.Length -gt 20)
+    {
+        $scrollText = $radioText + (" " * 20)
+        $start = $row2Offset % $scrollText.Length
+        if ($start + 20 -le $scrollText.Length)
+        {
+            $row2 = $scrollText.Substring($start, 20)
+        }
+        else
+        {
+            $row2 = $scrollText.Substring($start) + $scrollText.Substring(0, 20 - ($scrollText.Length - $start))
+        }
+    }
+    else
+    {
+        $row2 = $radioText.PadRight(20)
+    }
+
+    if ($row1 -ne $LASTROW1 -or $row2 -ne $LASTROW2)
+    {
+        $LASTROW1 = $row1
+        $LASTROW2 = $row2
+
+        Clear-TTYDisplay -DeviceName "/dev/ttyUSB0"
+        Write-TTYDisplayText -DeviceName "/dev/ttyUSB0" -Text ($row1 + $row2)
+    }
+}
+
+Write-Host "<ENTER>"
+Read-Host
